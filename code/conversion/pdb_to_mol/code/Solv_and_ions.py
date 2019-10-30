78ee7c68bdcbb7a401d1a080c46de4957f895634
Anna Carbery <anna.carbery@dtc.ox.ac.uk>
2019-10-30 12:28:06 +0000

    non_ligs now put in directory above

--- code/conversion/pdb_to_mol/code/Solv_and_ions.py
+++ code/conversion/pdb_to_mol/code/Solv_and_ions.py
@@ -4,4 +4,4 @@ import json
 import os
 
 non_ligs = ['DMS', 'CL', 'MG', 'EDO', 'HOH', 'PEG']
-json.dump(non_ligs, open(os.path.join("./conversion", "non_ligs"), "w"))
+json.dump(non_ligs, open(os.path.join("../", "non_ligs"), "w"))
