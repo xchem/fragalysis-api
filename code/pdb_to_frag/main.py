f9d258c226810c72cc6ef0b1d7a7ef5850a9a20a
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2019-11-20 12:14:36 +0000

    file directories in main :spaghetti:

--- code/pdb_to_frag/main.py
+++ code/pdb_to_frag/main.py
@@ -1,9 +1,13 @@
 from align import Align
 from validate import Validate
+import os
 
 if __name__ == "__main__":
-    a_dir = '../anna/Hard_example/'
-    out_dir = '../anna/aligned2/'
+    a_dir = '../../data/anna/input/ATAD2/'
+    out_dir = '../../data/output/ATAD2/'
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
     validation = Validate(a_dir)
     validation.validate_pdbs()
