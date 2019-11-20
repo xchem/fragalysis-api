36070ae1a7cbc82e462b100a2b1911bf9a6827b8
TobiasHeOl2 <ohsaibot@mail.com>
2019-11-20 10:18:14 +0000

    Merge branch '131119_branch'
    # Conflicts:
    #	.gitignore
    #	code/__pycache__/align.cpython-37.pyc
    #	code/__pycache__/validate.cpython-37.pyc
    #	code/align.py

--- code/main.py
+++ code/main.py
@@ -2,11 +2,11 @@ from align import Align
 from validate import Validate
 
 if __name__ == "__main__":
-    a_dir = '../data/ATAD2/'
-
+    a_dir = '../data/Hard_example/'
+    out_dir = '../data/aligned2/'
 
     validation = Validate(a_dir)
-    validation.validate_pdbs
+    validation.validate_pdbs()
 
     struc = Align(a_dir, pdb_ref='')
-    struc.align()
+    struc.align(out_dir)
