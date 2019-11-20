13773cfe03fef03fcd229d4369315434084ccb2c
Anna Carbery <anna.carbery@dtc.ox.ac.uk>
2019-11-20 12:28:21 +0000

    :white_check_mark: test updated!

--- tests/pdb_to_frag/test_conversion.py
+++ tests/pdb_to_frag/test_conversion.py
@@ -1,71 +1,72 @@
-import unittest
-from code import set_up
 import os
+import unittest
+from code.pdb_to_frag.Conversion_pdb_mol import set_up
+
 
 def test_open_file():
     """
     pdb file opens and checks that correct number of lines
     """
-    new = set_up("5q1j")
+    new = set_up("5q1j", 'anna')
     assert len(new.pdbfile) == 6779
 
 def test_make_directory():
     """
     tests that directory to receive results has been created
     """
-    new = set_up("5q1j")
-    assert os.path.isdir("../results/"+str(new.pdbcode))
+    new = set_up("5q1j", 'anna')
+    assert os.path.isdir(new.RESULTS_DIRECTORY)
 
 def test_hets_and_cons():
     """
     tests that the correct number of heteroatoms and conect lines have been found
     """
-    new = set_up("5q1j")
+    new = set_up("5q1j", 'anna')
     assert len(new.hetatms) == 326 and len(new.conects) == 24
 
 def test_remove_nonligs():
     """
     tests that solvents and ions from crystallography have been removed
     """
-    new = set_up("5q1j")
+    new = set_up("5q1j", 'anna')
     assert len(new.final_hets) == 11
 
 def test_wanted_ligs():
     """
     tests that the ligand identifiers at the top of the pdb file have been found
     """
-    new = set_up("5q1j")
+    new = set_up("5q1j", 'anna')
     assert len(new.wanted_ligs) != None
 
 def test_make_mol_objs():
     """
     tests that a molecular object has been made made for each ligand
     """
-    new = set_up("5qj7")
+    new = set_up("5qj7", 'anna')
     assert len(new.mol_lst) == 4
 
 def test_make_pdb_file():
     """
     tests that a pdb file has been made for a particular ligand with the correct number of lines (HETATM & CONECT)
     """
-    set_up("5qj7")
-    file = open("../results/5qj7/5qj7_JMM_A_303.pdb").readlines()
+    new = set_up("5qj7", 'anna')
+    file = open(new.RESULTS_DIRECTORY + "/5qj7_JMM_A_303.pdb").readlines()
     assert len(file) == 36
 
 def test_make_mol_file():
     """
     tests that a mol file has been made with the correct number of lines
     """
-    set_up("5qj7")
-    file = open("../results/5qj7/5qj7_JMM_A_303_mol.mol").readlines()
+    new = set_up("5qj7", 'anna')
+    file = open(new.RESULTS_DIRECTORY + "/5qj7_JMM_A_303_mol.mol").readlines()
     assert len(file) == 43
 
 def test_make_sdf_file():
     """
     test that a sdf file has been made that incorporates the different mol objects
     """
-    set_up("5qj7")
-    file = open("../results/5qj7/5qj7_out.sdf").readlines()
+    new = set_up("5qj7", 'anna')
+    file = open(new.RESULTS_DIRECTORY +"/5qj7_out.sdf").readlines()
     assert len(file) == 176
 
 
