c9ca93c4755d6ccc4a010560a04793755c8c6b7f
Anna Carbery <anna.carbery@dtc.ox.ac.uk>
2019-11-20 15:33:27 +0000

    :art: main file better

--- code/xcimporter/main.py
+++ code/xcimporter/main.py
@@ -7,13 +7,16 @@ if __name__ == "__main__":
     user_id = input("User ID:")
     in_dir = '../../data/xcimporter/input/'
     out_dir = '../../data/xcimporter/output/'
-    pdb_list = ['6epu', '6epv', '6epx', '6hi3']
+    pdb_list = []
+
+    for file in os.listdir(os.path.join(in_dir, str(user_id))):
+        pdb_list.append(file[:-4])
 
     if not os.path.exists(os.path.join(out_dir, str(user_id), '/')):
         os.makedirs(os.path.join(out_dir, str(user_id), '/'))
         os.makedirs(os.path.join(out_dir, str(user_id), 'tmp/'))
 
-    validation = Validate(in_dir)
+    validation = Validate(os.path.join(in_dir, str(user_id)))
     validation.validate_pdbs
 
     struc = Align(os.path.join(in_dir, str(user_id)), pdb_ref='')
