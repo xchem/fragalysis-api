db7bc66ee2655ce4e66f7970d072e6cbccca129a
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2019-11-27 12:03:22 +0000

    :panda_face:

--- code/xcimporter/main.py
+++ code/xcimporter/main.py
@@ -4,29 +4,37 @@ from validate import Validate
 from create_directory import create_directory
 import os
 from shutil import rmtree
+import argparse
 
 if __name__ == "__main__":
-    user_id = input("User ID:")
-    in_dir = '../../data/xcimporter/input/'
-    out_dir = '../../data/xcimporter/output/'
+    #make these commandline options using argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-id", "--user_id", help="specify user ID", required = True)
+    parser.add_argument("-i", "--in_dir", default='../../data/xcimporter/input/', help="specify the directory the PDB files is in", required = False)
+    parser.add_argument("-o", "--out_dir", default='../../data/xcimporter/output/', help="specify the directory to output the xchem format PDBs to", required = False)
+    args = parser.parse_args()
+    if args.in_dir == '../../data/xcimporter/input/':
+        print('Using the default input directory ', args.in_dir)
+    if args.out_dir == '../../data/xcimporter/output/':
+        print('Using the default input directory ', args.out_dir)
     pdb_list = []
 
-    for file in os.listdir(os.path.join(in_dir, str(user_id))):
+    for file in os.listdir(os.path.join(args.in_dir, str(args.user_id))):
         pdb_list.append(file[:-4])
 
-    if not os.path.exists(os.path.join(out_dir, str(user_id), '/')):
-        os.makedirs(os.path.join(out_dir, str(user_id), '/'))
-        os.makedirs(os.path.join(out_dir, str(user_id), 'tmp/'))
+    if not os.path.exists(os.path.join(args.out_dir, str(args.user_id), '/')):
+        os.makedirs(os.path.join(args.out_dir, str(args.user_id), '/'))
+        os.makedirs(os.path.join(args.out_dir, str(args.user_id), 'tmp/'))
 
-    validation = Validate(os.path.join(in_dir, str(user_id)))
+    validation = Validate(os.path.join(args.in_dir, str(args.user_id)))
     validation.validate_pdbs
 
-    struc = Align(os.path.join(in_dir, str(user_id)), pdb_ref='')
-    struc.align(os.path.join(out_dir, str(user_id), 'tmp/'))
+    struc = Align(os.path.join(args.in_dir, str(args.user_id)), pdb_ref='')
+    struc.align(os.path.join(args.out_dir, str(args.user_id), 'tmp/'))
 
     for i in pdb_list:
-        new = set_up(i, str(user_id))
+        new = set_up(i, str(args.user_id))
 
-    create_directory(str(user_id), os.path.join(out_dir, str(user_id), 'tmp/'))
+    create_directory(str(args.user_id), os.path.join(args.out_dir, str(args.user_id), 'tmp/'))
 
-    rmtree(os.path.join(out_dir, str(user_id), 'tmp/'))
+    rmtree(os.path.join(args.out_dir, str(args.user_id), 'tmp/'))
