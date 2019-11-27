5889eb1be18ab8ff1ef8136ce3fdbec71d744220
TobiasHeOl2 <ohsaibot@mail.com>
2019-11-27 12:08:10 +0000

    Merge branch '131119_branch'
    # Conflicts:
    #	.gitignore
    #	code/__pycache__/align.cpython-37.pyc
    #	code/__pycache__/validate.cpython-37.pyc
    #	code/align.py

---
+++ code/xcimporter/xcimporter.py
@@ -0,0 +1,57 @@
+from align import Align
+from conversion_pdb_mol import set_up
+from validate import Validate
+from create_directory import create_directory
+import os
+from shutil import rmtree
+import argparse
+
+
+def xcimporter(user_id, in_dir, out_dir):
+    """Formats a lists of PDB files into fragalysis friendly format.
+    1. Validates the naming of the pdbs.
+    2. It aligns the pdbs (_bound.pdb file).
+    3. It cleans the pdbs (removes solvents and ions).
+    4. Splits the pdbs into ligands (.mol, .sdf and .pdb formats) and protein files (_apo.pdb file).
+    5. Orders all the files into the correct directory structure required for fragalysis.
+
+    :param user_id: data ID given to the input. Should be present in in_dir.
+    :param in_dir: Directory containing data ID directories.
+    :param out_dir: Directory containing processed pdbs (will be created if it doesn't exists).
+    :return:
+    """
+    pdb_list = []
+
+    for file in os.listdir(os.path.join(in_dir, str(user_id))):
+        pdb_list.append(file[:-4])
+
+    if not os.path.exists(os.path.join(out_dir, str(user_id), '/')):
+        os.makedirs(os.path.join(out_dir, str(user_id), '/'))
+        os.makedirs(os.path.join(out_dir, str(user_id), 'tmp/'))
+
+    validation = Validate(os.path.join(in_dir, str(user_id)))
+    validation.validate_pdbs
+
+    struc = Align(os.path.join(in_dir, str(user_id)), pdb_ref='')
+    struc.align(os.path.join(out_dir, str(user_id), 'tmp/'))
+
+    for i in pdb_list:
+        new = set_up(i, str(user_id))
+
+    create_directory(str(user_id), os.path.join(out_dir, str(user_id), 'tmp/'))
+
+    rmtree(os.path.join(out_dir, str(user_id), 'tmp/'))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-id', '--user_id', required=True, help='Description for foo argument')
+    parser.add_argument('-i', '--in_dir', default='../../data/xcimporter/input/', help='Description for bar argument')
+    parser.add_argument('-o', '--out_dir', default='../../data/xcimporter/output/', help='Description for bar argument')
+    args = vars(parser.parse_args())
+
+    user_id = args['user_id']
+    in_dir = args['in_dir']
+    out_dir = args['out_dir']
+
+    xcimporter(user_id, in_dir, out_dir)
