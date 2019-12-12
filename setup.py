e0e95290b93058e56af08a1b886086992b901688
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2019-12-12 16:55:20 +0000

    :panda_face:

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 setup(
     name='fragalysis_api',
     version='0.0.1',
-  #  install_requires=requirements,
+  # install_requires=requirements,
     author='Fragment 5',
     packages=find_packages(include=('fragalysis_api', 'fragalysis_api.*')),
     )
