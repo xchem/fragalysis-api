7c4735c1657f90481c54101ab0821bbeabcb5ea5
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2019-12-12 16:52:35 +0000

    :panda_face: failing at requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+
+#with open('requirements.txt') as f:
+ #   requirements = f.read().splitlines()
+
+
 setup(
     name='fragalysis_api',
     version='0.0.1',
+  #  install_requires=requirements,
     author='Fragment 5',
     packages=find_packages(include=('fragalysis_api', 'fragalysis_api.*')),
     )
