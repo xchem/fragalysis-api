d2d61d735f912a49f4a6d1de35ab70195ee4de11
Anna Carbery <anna.carbery@dtc.ox.ac.uk>
2019-12-16 16:36:41 +0000

    download files from PDB :whale:

---
+++ fragalysis_api/xcimporter/pdbimporter.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+from Bio.PDB import PDBList
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-id', '--user_id', required=True, help='user ID')
+parser.add_argument('-pdb', '--pdb', required=True, help='pdb code you want to import')
+args = vars(parser.parse_args())
+
+user_id = args['user_id']
+data_dir = os.path.join('..', '..', 'data', 'xcimporter', 'input')
+pdb_code = args['pdb']
+
+if not os.path.exists(os.path.join(data_dir, user_id)):
+    os.mkdir(os.path.join(data_dir, user_id))
+    print('making directory')
+
+if not os.path.exists(os.path.join(data_dir, user_id, pdb_code+'.pdb')):
+    pdbl = PDBList()
+    pdbl.retrieve_pdb_file(pdb_code, pdir = os.path.join(data_dir, user_id), file_format = 'pdb')
+    os.rename(os.path.join(data_dir, user_id, 'pdb'+pdb_code+'.ent'), os.path.join(data_dir, user_id, pdb_code+'.pdb'))
+else:
+    print('File is already downloaded')
