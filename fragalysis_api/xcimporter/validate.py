e19771aaebe5b6da0a7f4d255b4b8df9fed0cfbe
TobiasHeOl2 <ohsaibot@mail.com>
2019-12-17 15:28:18 +0000

    :cow: Updated tests + test data

--- fragalysis_api/xcimporter/validate.py
+++ fragalysis_api/xcimporter/validate.py
@@ -2,7 +2,6 @@ import glob
 import os
 import string
 
-
 class Validate:
     def __init__(self, directory):
         self.directory = directory
@@ -10,18 +9,30 @@ class Validate:
 
     @property
     def is_pdbs_valid(self):
+        """
+        Checks whether all pdbs are valid. If just one isn't it returns False
+
+        :return: boolean answer
+        """
         return not bool(self._fail_list)
 
     @property
     def validate_pdbs(self):
+        """
+        List of all pdbs which failed validation
+
+        :return: list of all pdbs which failed validation
+        """
         return self._fail_list
 
     @validate_pdbs.setter
     def validate_pdbs(self, pdb_file_list):
-    
+
         fail_list = []
         for pdb in pdb_file_list:
-            fail_list += [i for i in ValidatePDB(pdb).PDB_validations() if i is not None]
+            pdb_val = ValidatePDB(pdb)
+            if not pdb_val.is_pdb_valid:
+                fail_list.append(pdb_val.pdb_name)
 
         self._fail_list = fail_list
 
@@ -35,10 +46,20 @@ class Validate:
 
     @property
     def does_dir_exist(self):
+        """
+        Checks if the directory exists.
+
+        :return: boolean answer
+        """
         return bool(os.path.isdir(self.directory))
 
     @property
     def is_there_a_pdb_in_dir(self):
+        """
+        Checks if there is at least one pdb file in the directory.
+
+        :return: boolean answer
+        """
         return bool(self.get_files)
 
 
@@ -48,15 +69,25 @@ class ValidatePDB:
         self.pdb_file = pdb_file
         self.pdb_name = os.path.splitext(os.path.basename(pdb_file))[0]
 
+    @property
+    def is_pdb_valid(self):
+        """
+        If the pdb has failed just 1 check return False
+        """
+        return not (False in self._PDB_validations)
 
-    def PDB_validations(self):
-        yield self.is_pdb_to_large()
-        yield self.does_pdb_name_contain_none_whitelist_char()
-        yield self.is_pdb_name_within_range()
-        yield self.does_pdb_have_same_amount_of_protein_chains()
+    @property
+    def _PDB_validations(self):
+        """
+        Yields the results from the different types of validations.
+        """
+        yield self.is_pdb_allowed_size
+        yield self.does_pdb_name_contain_only_whitelist_char
+        yield self.is_pdb_name_within_size_limit
+        # yield self.does_pdb_have_same_amount_of_protein_chains
 
-
-    def is_pdb_to_large(self):
+    @property
+    def is_pdb_allowed_size(self):
         """Checks whether a pdb file is larger than the allowed file size limit.
 
         :return: name of pdb if it failed to be smaller than the limit.
@@ -65,13 +96,16 @@ class ValidatePDB:
 
         if os.path.getsize(self.pdb_file) <= 0:
             print(f'{self.pdb_name} file is to small.')
-            return self.pdb_name
+            return False
 
         if os.path.getsize(self.pdb_file) / 1000000 > file_size_limit:
             print(f'{self.pdb_name} file larger than limit of {file_size_limit}mb.')
-            return self.pdb_name
+            return False
 
-    def does_pdb_name_contain_none_whitelist_char(self):
+        return True
+
+    @property
+    def does_pdb_name_contain_only_whitelist_char(self):
         """Checks whether a pdb files name contains characters which are not allowed.
 
         :return: name of pdb if it contains disallowed characters.
@@ -80,9 +114,12 @@ class ValidatePDB:
             if character not in string.ascii_letters+string.digits:
                 print(f'{self.pdb_name} contains characters which are not allowed.\n'
                       f'Please only use ASCII or digits.')
-                return self.pdb_name
+                return False
 
-    def is_pdb_name_within_range(self):
+        return True
+
+    @property
+    def is_pdb_name_within_size_limit(self):
         """Checks whether the name of a pdb file is within the allowed range.
 
         :return: name of pdb if it was outside the allowed range.
@@ -93,8 +130,11 @@ class ValidatePDB:
         if (len(self.pdb_name) > char_max) or (len(self.pdb_name) < char_min):
             print(f'{self.pdb_name} has a name outside the allowed length range.\n'
                   f'Please keep names between {char_min}-{char_max} characters.')
-            return self.pdb_name
+            return False
 
+        return True
+
+    @property
     def does_pdb_have_same_amount_of_protein_chains(self):
         pass
 
--- tests/xcimporter/test_validate.py
+++ tests/xcimporter/test_validate.py
@@ -2,16 +2,13 @@ import unittest
 import os
 from fragalysis_api import Validate, ValidatePDB
 
-cwd = os.getcwd()
-path = os.path.split(cwd)
 
-data_path = os.path.join('data', 'xcimporter', 'input') #str(path[0]), '..',
-good_directory = os.path.join(data_path, 'ATAD2')
-cif_directory = os.path.join(data_path, 'CIF')
-Hard_directory = os.path.join(data_path, 'Hard_example')
-Semi_hard_directory = os.path.join(data_path, 'Semi_hard_examples')
-PDB_directory = os.path.join(data_path, 'PDB')
-str(path[0]), '..',
+
+data_path = os.path.join('data', 'xcimporter', 'input')
+good_dir = os.path.join(data_path, 'ATAD2')
+test_dir = os.path.join(data_path, 'examples_to_test1')
+test2_dir = os.path.join(data_path, 'examples_to_test2')
+PDB_dir = os.path.join(data_path, 'PDB')
 
 class Validate_test(unittest.TestCase):
 
@@ -19,27 +16,74 @@ class Validate_test(unittest.TestCase):
         """
         This test shows how it should work when the input is correct
         """
-        print
-        val_obj = Validate(good_directory)
+        val_obj = Validate(good_dir)
         # Test that there is no faults with this input
         self.assertTrue(val_obj.is_pdbs_valid)
         self.assertTrue(not bool(val_obj.validate_pdbs))
-        # Test that the dir contains files (it doesnt return a mistake)
+        # If dir exists
         self.assertTrue(val_obj.does_dir_exist)
+        # If there is a pdb in dir
         self.assertTrue(val_obj.is_there_a_pdb_in_dir)
+        # Does it get all the files?
+        self.assertEqual(val_obj.get_files, [os.path.join(good_dir, '6epv.pdb'), os.path.join(good_dir, '6hi3.pdb'),
+                                             os.path.join(good_dir, '6epx.pdb'), os.path.join(good_dir, '6epu.pdb')])
 
-    def test_cif(self):
+
+    def test_miscellanea(self):
         """
         When the input is a cif file + no pdbs in the directory
         """
-        cif_obj = Validate(cif_directory)
-        # Test that there is no faults with this input
-        self.assertTrue(cif_obj.is_pdbs_valid)
-        self.assertTrue(not bool(cif_obj.validate_pdbs))
-        # Test that the dir contains files (it doesnt return a mistake)
-        self.assertTrue(cif_obj.does_dir_exist)
-        self.assertFalse(cif_obj.is_there_a_pdb_in_dir)
+        misc_obj = Validate('not_a_dir')
+        # Check pdbs are valid (there are no pdbs so it should pass)
+        self.assertTrue(misc_obj.is_pdbs_valid)
+        self.assertTrue(not bool(misc_obj.validate_pdbs))
+        # If dir exists
+        self.assertFalse(misc_obj.does_dir_exist)
+        # That there are no pdbs in dir
+        self.assertFalse(misc_obj.is_there_a_pdb_in_dir)
 
+    def test_mistake_catching(self):
+        """
+        Most of these cases should raise errors
+        """
+        hard_obj = Validate(test_dir)
+        # If dir exists
+        self.assertTrue(hard_obj.does_dir_exist)
+        # If there are a pdb in dir
+        self.assertTrue(hard_obj.is_there_a_pdb_in_dir)
+        # Check all files are retrieved
+        self.assertEqual(hard_obj.get_files,
+                         [os.path.join(test_dir, '5g1n (copy).pdb'), os.path.join(test_dir, '1.pdb'),
+                          os.path.join(test_dir, '5g1n.pdb'), os.path.join(test_dir, 'To_big_pdb.pdb'),
+                          os.path.join(test_dir, '5g1p (copy).pdb'), os.path.join(test_dir, '5g1o.pdb'),
+                          os.path.join(test_dir, 'Empty_pdb.pdb'), os.path.join(test_dir, '1234567890asdfghjklqwe.pdb'),
+                          os.path.join(test_dir, '5g1p.pdb'), os.path.join(test_dir, '5xzc.pdb')])
+        # Check it catches that there are invalid pdbs in the data set
+        self.assertFalse(hard_obj.is_pdbs_valid)
+        # Ensure it caught them all
+        self.assertEqual(hard_obj.validate_pdbs, ['5g1n (copy)', '1', 'To_big_pdb', '5g1p (copy)',
+                                                  'Empty_pdb', '1234567890asdfghjklqwe'])
+        # Check if whitelisting works
+        self.assertFalse(
+            ValidatePDB(os.path.join(test_dir, '5g1n (copy).pdb')).does_pdb_name_contain_only_whitelist_char)
+        self.assertFalse(
+            ValidatePDB(os.path.join(test_dir, '5g1p (copy).pdb')).does_pdb_name_contain_only_whitelist_char)
+        # Check if it finds empty pdb files
+        self.assertFalse(
+            ValidatePDB(os.path.join(test_dir, 'Empty_pdb.pdb')).is_pdb_allowed_size)
+        # Check if it finds to large files
+        self.assertFalse(
+            ValidatePDB(os.path.join(test_dir, 'To_big_pdb.pdb')).is_pdb_allowed_size)
+        # Check if it finds pdbs with too small names
+        self.assertFalse(
+            ValidatePDB(os.path.join(test_dir, '1.pdb')).is_pdb_name_within_size_limit)
+        # Check if it finds pdbs with too large names
+        self.assertFalse(
+            ValidatePDB(os.path.join(test_dir, '1234567890asdfghjklqwe.pdb')).is_pdb_name_within_size_limit)
+
+
+    def test_semi_hard(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
