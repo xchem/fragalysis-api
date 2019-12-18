f437e5afae3a7156ede502301210b9ebcc649ddc
TobiasHeOl2 <ohsaibot@mail.com>
2019-12-18 15:01:30 +0000

    Small updates

--- tests/xcimporter/test_align.py
+++ tests/xcimporter/test_align.py
@@ -20,10 +20,11 @@ directory = os.path.join(str(path[0]), ATAD2_directory)
 class AlignTest(unittest.TestCase):
 
 
-    # def test_get_files(self):
-    #     files = ['6epu.pdb', '6epv.pdb', '6epx.pdb', '6hi3.pdb']
-    #     whole_files = sorted([os.path.join(self.align_obj.directory, x) for x in files])
-    #     self.assertEqual(self.align_obj.pdb_in_list, whole_files)
+    def test_get_files(self):
+        #    files = ['6epu.pdb', '6epv.pdb', '6epx.pdb', '6hi3.pdb']
+        #     whole_files = sorted([os.path.join(self.align_obj.directory, x) for x in files])
+        #     self.assertEqual(self.align_obj.pdb_in_list, whole_files)
+        pass
 
     def test_load_success(self):
         #align_obj = Align(ATAD2_directory)
