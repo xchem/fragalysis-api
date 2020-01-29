788d8ea212cd6225b69d2843ef757b4f8fb1cbc5
Anna Carbery <anna.carbery@dtc.ox.ac.uk>
2020-01-29 11:42:31 +0000

    improving coverage3 :whale:

--- tests/xcimporter/test_pdb_query.py
+++ tests/xcimporter/test_pdb_query.py
@@ -14,7 +14,7 @@ class ValidateTest(unittest.TestCase):
         assert(len(query_obj.match_ligs) == 41)
         query_obj.print_number_ligs()
         query_obj.view_ligands()
-        query_obj.save_dictionary('anna')
+ #       query_obj.save_dictionary('anna')
  #       assert(os.path.exists(os.path.join('..', '..', 'data', 'xcimporter', 'other', 'anna')))
 
 if __name__ == '__main__':
