3823ad54fbb17c802774aaeb3c63a0ccc8e6c484
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2020-02-05 10:47:38 +0000

    packages

--- fragalysis_api/__init__.py
+++ fragalysis_api/__init__.py
@@ -3,6 +3,6 @@ from .xcimporter.align import Align
 from .xcimporter.conversion_pdb_mol import set_up
 from .xcimporter.xc_utils import *
 from .xcimporter.xcimporter import xcimporter
-from .xcextracter.get_target_data import GetTargetsData
-from .xcextracter.get_target_data import GetPdbData
-from .xcextracter.get_target_data import GetMoleculesData
+from .xcextracter.getdata import GetTargetsData
+from .xcextracter.getdata import GetPdbData
+from .xcextracter.getdata import GetMoleculesData
