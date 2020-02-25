3d7dcf75fa3da0d1de2ef8fedc6ecd23b57ee686
Maranga <marangamokaya@JMM-Pro.local>
2020-02-25 10:49:51 +0000

    documentation

---
+++ docs/html/search/all_d.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['set_5fmol_5fdata_32',['set_mol_data',['../classfragalysis__api_1_1xcextracter_1_1getdata_1_1_get_molecules_data.html#ab1f0b5b8edbd496d800e08c7f8758a53',1,'fragalysis_api::xcextracter::getdata::GetMoleculesData']]],
+  ['set_5fmolecule_5furl_33',['set_molecule_url',['../classfragalysis__api_1_1xcextracter_1_1getdata_1_1_get_molecules_data.html#a61f7510bc705fab0ee33128872619f40',1,'fragalysis_api::xcextracter::getdata::GetMoleculesData']]],
+  ['set_5ftarget_5fid_34',['set_target_id',['../classfragalysis__api_1_1xcextracter_1_1getdata_1_1_get_molecules_data.html#acc782fb40405a434903f6d3e80624c9c',1,'fragalysis_api::xcextracter::getdata::GetMoleculesData']]],
+  ['set_5ftarget_5fname_5furl_35',['set_target_name_url',['../classfragalysis__api_1_1xcextracter_1_1getdata_1_1_get_targets_data.html#abf5179a2489b7edaa16dc6bc8c89f051',1,'fragalysis_api::xcextracter::getdata::GetTargetsData']]]
+];
