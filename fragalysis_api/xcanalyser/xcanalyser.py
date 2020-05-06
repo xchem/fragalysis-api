3d9acf77d975ff24e81c12fd84938ea9d5c34449
Tobias Hegelund Olsen <tobiasheol@gmail.com>
2020-05-06 11:36:00 +0100

    adjusted analysers

--- fragalysis_api/xcanalyser/xcanalyser.py
+++ fragalysis_api/xcanalyser/xcanalyser.py
@@ -3,7 +3,13 @@ from rdkit import Chem
 from fragalysis_api import xcgraphcreator
 
 def xcanalyser():
+    
+    #def __init__(self, graphs):        
+    #    self.graphs = graphs
+        
+    
 
+    #base_graph = graphs
     #m = Chem.MolFromMolFile('../..data/xcimporter/output/ATAD')
 
     return xcgraphcreator(target_smiles='O=C(Nc1ccccc1)Nc1cccnc1')
