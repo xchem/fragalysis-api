ede16149a58ed3e02df302577f237d10fd339898
Rachael Skyner <reskyner@googlemail.com>
2020-07-15 09:42:40 +0100

    add ability to get bound pdb files

--- tests/xcextractor/test_GetPdbData.py
+++ tests/xcextractor/test_GetPdbData.py
@@ -8,9 +8,9 @@ class GetPdbDataTest(unittest.TestCase):
         search = GetPdbData()
         self.assertIsNotNone(search)
 
-    def test_get_pdb_file(self):
+    def test_get_apo_pdb_file(self):
         search = GetPdbData()
-        self.assertIsNotNone(search.get_pdb_file('NUDT5A-x0114_1'))
+        self.assertIsNotNone(search.get_apo_pdb_file('NUDT5A-x0114_1'))
 
 if __name__ == '__main__':
     unittest.main()
