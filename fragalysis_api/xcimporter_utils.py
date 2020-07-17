ade0f1658bc21862d13eae5c203860494aa47533
Maranga Mokaya <joshua.mokaya@dtc.ox.ac.uk>
2020-07-17 14:43:01 +0100

    cleaning branch

--- fragalysis_api/xcimporter_utils.py
+++
@@ -1,4 +0,0 @@
-from fragalysis_api.xcimporter.align import *
-from fragalysis_api.xcimporter.conversion_pdb_mol import *
-from fragalysis_api.xcimporter.validate import *
-from fragalysis_api.xcimporter.xc_utils import *
