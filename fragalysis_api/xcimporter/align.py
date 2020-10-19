c1dda730f3ae75f42b1b9b4494c5a95c92e7b438
tjgorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-10-19 11:35:53 +0100

    add prints

--- fragalysis_api/xcimporter/align.py
+++ fragalysis_api/xcimporter/align.py
@@ -323,8 +323,9 @@ class Structure:
                 for res_self in chain.get_polymer():
                     current_res_id = ResidueID.from_residue_chain(model, chain, res_self)
                     res_other = other.structure[current_res_id.model][current_res_id.chain][current_res_id.insertion][0]
-                    #res_other = other.structure[current_res_id][0]
-
+                    print(f'{self.structure}|{res_self}')
+                    print(f'{other.structure}|{res_other}')
+                          
                     self_ca_pos = res_self["CA"][0].pos
                     other_ca_pos = res_other["CA"][0].pos
 
