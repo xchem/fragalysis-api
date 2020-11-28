2e19be332b74b5df077dc416038036cad29d6d73
Rachael Skyner <reskyner@googlemail.com>
2020-11-28 18:48:32 +0000

    add new functionality to api init

--- fragalysis_api/__init__.py
+++ fragalysis_api/__init__.py
@@ -8,6 +8,7 @@ from .xcimporter.xcimporter import xcimporter
 from .xcextracter.getdata import GetTargetsData, GetMoleculesData, GetPdbData
 from .xcextracter.frag_web_live import can_connect
 from .xcextracter.xcextracter import xcextracter
+from .xcextracter.sites import GetMolgroupData
 
 from .xcanalyser.graphcreator import GraphRequest, xcgraphcreator
 from .xcanalyser.xcanalyser import xcanalyser
