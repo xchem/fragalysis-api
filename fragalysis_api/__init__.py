6ee9e038829869672b669ecf0318c648ca20685f
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-04-29 13:03:53 +0100

    Updating init.py

--- fragalysis_api/__init__.py
+++ fragalysis_api/__init__.py
@@ -1,7 +1,7 @@
 from .xcglobalscripts.set_config import ConfigSetup
 
 from .xcimporter.validate import Validate, ValidatePDB
-from .xcimporter.conversion_pdb_mol import Ligand, pdb_apo, set_up
+from .xcimporter.conversion_pdb_mol import Ligand, pdb_apo, set_up, convert_small_AA_chains, copy_extra_files
 from .xcimporter.align import Align, Monomerize
 from .xcimporter.xc_utils import to_fragalysis_dir
 from .xcimporter.xcimporter import xcimporter
