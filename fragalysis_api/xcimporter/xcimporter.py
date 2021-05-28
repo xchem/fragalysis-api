9293ff184ff7dce421ca8fc2b9668fc61e9d5a2d
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-05-28 16:14:40 +0100

    rrf implementation

--- fragalysis_api/xcimporter/xcimporter.py
+++ fragalysis_api/xcimporter/xcimporter.py
@@ -13,7 +13,8 @@ from fragalysis_api import set_up, convert_small_AA_chains, copy_extra_files
 
 from distutils.dir_util import copy_tree
 
-def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomerize=False, biomol=None, covalent=False,
+
+def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, reduce_reference_frame=False, biomol=None, covalent=False,
                pdb_ref="", max_lig_len=0):
     """Formats a lists of PDB files into fragalysis friendly format.
     1. Validates the naming of the pdbs.
@@ -28,12 +29,12 @@ def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomeri
     :param target: Name of the folder to be created inside out_dir
     :param metadata: If set to 1 will create a csv file called metadata.csv in target directory
     :param validate: Validates, and explicitly, warns if input PDB files are not suitable
-    :param monomerize: Bool, if True, will attempt to split pdb files into seperate chains
+    :param reduce_reference_frame: Boolean, if True, will attempt to the number of reference frames by aligning to the first chain in the reference file.
     :param biomol: plain-text file containing header information about the bio-molecular
-        context of the pdb structures. If provided the contents will be appended to the top of the _apo.pdb files
+        context of the pdb structures. If provided the contents will be appended to the top of the _apo.pdb files, Now defunct?
     :param covalent: Bool, if True, will attempt to convert output .mol files to account for potential covalent attachments
     :pdb_ref: String, if provided, all pdb files will be aligned to the name of the file (sans extnesion) that is specified.
-    :max_lig_len: Integer, If >0 will convert all chains with fewer than max_lig_len residues to HETATM with the name LIG.
+    :max_lig_len: Integer, If >0 will convert all chains with fewer than max_lig_len residues to HETATM with the name LIG. [Currently broken, yikes]
     :return: Hopefully, beautifully aligned files that be used with the fragalysis loader :)
     """
 
@@ -63,25 +64,28 @@ def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomeri
 
     # Experimental - if option is used then chains are converted. All files need to be moved????
     if int(max_lig_len) > int(0):
-        print(f'EXPERIMENTAL: Converting all chains with less than {max_lig_len} residues to HETATM LIG')
+        print(
+            f'EXPERIMENTAL: Converting all chains with less than {max_lig_len} residues to HETATM LIG')
         out = os.path.join(out_dir, f'maxliglen{target}/')
         if not os.path.isdir(out):
             os.makedirs(out)
         infiles = glob.glob(os.path.join(in_dir, '*.pdb'))
         for i in infiles:
-            convert_small_AA_chains(in_file=i, out_file=os.path.join(out, os.path.basename(i)), max_len=max_lig_len)
+            convert_small_AA_chains(in_file=i, out_file=os.path.join(
+                out, os.path.basename(i)), max_len=max_lig_len)
             copy_extra_files(in_file=i, out_dir=out)
         in_dir = out
         print(in_dir)
 
-    if monomerize:
-        print("Monomerizing input structures")
-        out = os.path.join(out_dir, f'mono{target}/')
-        if not os.path.isdir(out):
-            os.makedirs(out)
-        mono = Monomerize(directory=in_dir, outdir=out)
-        mono.monomerize_all()
-        in_dir = out
+    # Who doesn't like zombie code...
+    # if monomerize:
+    #    print("Monomerizing input structures")
+    #    out = os.path.join(out_dir, f'mono{target}/')
+    #    if not os.path.isdir(out):
+    #        os.makedirs(out)
+    #    mono = Monomerize(directory=in_dir, outdir=out)
+    #    mono.monomerize_all()
+    #    in_dir = out
 
     pdb_smiles_dict = {'pdb': [], 'smiles': []}
 
@@ -90,27 +94,31 @@ def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomeri
             pdb_smiles_dict['pdb'].append(os.path.join(in_dir, f))
             print(os.path.join(in_dir, f).replace('.pdb', '_smiles.txt'))
             if os.path.isfile(os.path.join(in_dir, f).replace('.pdb', '_smiles.txt')):
-                pdb_smiles_dict['smiles'].append(os.path.join(in_dir, f).replace('.pdb', '_smiles.txt'))
+                pdb_smiles_dict['smiles'].append(os.path.join(
+                    in_dir, f).replace('.pdb', '_smiles.txt'))
             else:
                 pdb_smiles_dict['smiles'].append(None)
 
     print(pdb_smiles_dict['smiles'])
     print("Aligning protein structures")
-    print('New Stuff')
-    structure = Align(directory=in_dir, pdb_ref=pdb_ref, mono=monomerize)
+    structure = Align(directory=in_dir, pdb_ref=pdb_ref,
+                      rrf=reduce_reference_frame)
     structure.align(out_dir=os.path.join(out_dir, f"tmp{target}"))
 
     for smiles_file in pdb_smiles_dict['smiles']:
         if smiles_file:
             print(smiles_file)
-            copyfile(smiles_file, os.path.join(os.path.join(out_dir, f"tmp{target}", smiles_file.split('/')[-1])))
-            print(os.path.join(out_dir, f"tmp{target}", smiles_file.split('/')[-1]))
+            copyfile(smiles_file, os.path.join(os.path.join(
+                out_dir, f"tmp{target}", smiles_file.split('/')[-1])))
+            print(os.path.join(
+                out_dir, f"tmp{target}", smiles_file.split('/')[-1]))
 
     aligned_dict = {'bound_pdb': [], 'smiles': []}
 
     for f in os.listdir(os.path.join(out_dir, f"tmp{target}")):
         if '.pdb' in f:
-            aligned_dict['bound_pdb'].append(os.path.join(out_dir, f"tmp{target}", f))
+            aligned_dict['bound_pdb'].append(
+                os.path.join(out_dir, f"tmp{target}", f))
             if os.path.isfile(os.path.join(out_dir, f"tmp{target}", f).replace('_bound.pdb', '_smiles.txt')):
                 aligned_dict['smiles'].append(
                     os.path.join(out_dir, f"tmp{target}", f).replace('_bound.pdb', '_smiles.txt'))
@@ -126,7 +134,7 @@ def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomeri
                 _ = set_up(target_name=target,
                            infile=os.path.abspath(aligned),
                            out_dir=out_dir,
-                           monomerize=monomerize,
+                           rrf=reduce_reference_frame,
                            smiles_file=os.path.abspath(smiles),
                            biomol=biomol,
                            covalent=covalent,
@@ -136,7 +144,7 @@ def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomeri
                 _ = set_up(target_name=target,
                            infile=os.path.abspath(aligned),
                            out_dir=out_dir,
-                           monomerize=monomerize,
+                           rrf=reduce_reference_frame,
                            biomol=biomol,
                            covalent=covalent,
                            keep_headers=False)
@@ -167,7 +175,8 @@ def xcimporter(in_dir, out_dir, target, metadata=False, validate=False, monomeri
     copy_tree(in_dir2, os.path.join(out_dir, target, 'crystallographic'))
 
     # Time to use a for loop?
-    clean_up = [os.path.join(out_dir, f'maxliglen{target}'), os.path.join(out_dir, f'mono{target}'), os.path.join(out_dir, f'tmp{target}')]
+    clean_up = [os.path.join(out_dir, f'maxliglen{target}'), os.path.join(
+        out_dir, f'mono{target}'), os.path.join(out_dir, f'tmp{target}')]
     [shutil.rmtree(x) for x in clean_up if os.path.exists(x)]
 
     print("Files are now in a fragalysis friendly format!")
@@ -194,12 +203,15 @@ if __name__ == "__main__":
         "-v", "--validate", action="store_true", default=False, help="Validate input"
     )
     parser.add_argument(
-        "-m", "--monomerize", action="store_true", default=False, help="Monomerize input"
+        "-rrf", "--reduce_reference_frame", action="store_true", default=False, help="Reduce Reference frame to single chain?"
     )
     parser.add_argument("-t", "--target", help="Target name", required=True)
-    parser.add_argument("-md", "--metadata", action="store_true", help="Metadata output", default=False)
-    parser.add_argument("-b", "--biomol_txt", help="Biomol Input txt file", required=False, default=None)
-    parser.add_argument('-r', '--reference', help='Reference Structure', required=False, default=None)
+    parser.add_argument("-md", "--metadata", action="store_true",
+                        help="Metadata output", default=False)
+    parser.add_argument(
+        "-b", "--biomol_txt", help="Biomol Input txt file", required=False, default=None)
+    parser.add_argument(
+        '-r', '--reference', help='Reference Structure', required=False, default=None)
     parser.add_argument("-c",
                         "--covalent",
                         action="store_true",
@@ -218,7 +230,7 @@ if __name__ == "__main__":
     in_dir = args["in_dir"]
     out_dir = args["out_dir"]
     validate = args["validate"]
-    monomerize = args["monomerize"]
+    reduce_reference_frame = args["reduce_reference_frame"]
     target = args["target"]
     metadata = args["metadata"]
     biomol = args["biomol_txt"]
@@ -241,21 +253,21 @@ if __name__ == "__main__":
                out_dir=out_dir,
                target=target,
                validate=validate,
-               monomerize=monomerize,
+               reduce_reference_frame=reduce_reference_frame,
                metadata=metadata,
                biomol=biomol,
                covalent=covalent,
                pdb_ref=reference,
                max_lig_len=mll)
-    #This need replacing.
+    # This need replacing.
     #fix_pdb = open(os.path.join(out_dir, target, 'aligned', 'pdb_file_failures.txt'), 'w')
 
-    #for target_file in os.listdir(os.path.join(out_dir, target, 'aligned')):
+    # for target_file in os.listdir(os.path.join(out_dir, target, 'aligned')):
     #    if target_file != 'pdb_file_failures.txt' and len(os.listdir(os.path.join(out_dir, target, target_file))) < 2:
     #        rmtree(os.path.join(out_dir, target, target_file))
     #        fix_pdb.write(target_file.split('-')[1] + '\n')
 
-    #fix_pdb.close()
-    #print('For files that we were unable to process, look at the pdb_file_failures.txt file in your results directory.'
+    # fix_pdb.close()
+    # print('For files that we were unable to process, look at the pdb_file_failures.txt file in your results directory.'
     #      ' These files were unable to produce RDKit molecules, so the error likely lies in the way the ligand atoms or'
     #      'the conect files have been written in the pdb file')
