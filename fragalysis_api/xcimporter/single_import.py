4bd73742df9f9be9077c6236b2456b41fb0f0f69
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-06-09 12:37:59 +0100

    single import

--- fragalysis_api/xcimporter/single_import.py
+++ fragalysis_api/xcimporter/single_import.py
@@ -6,7 +6,7 @@ import shutil
 from fragalysis_api import Align, Monomerize, set_up, convert_small_AA_chains, copy_extra_files
 
 
-def import_single_file(in_file, out_dir, target, monomerize, reference_pdb, biomol=None, covalent=False, self_ref=False, max_lig_len=0):
+def import_single_file(in_file, out_dir, target, reduce_reference_frame, reference_pdb, biomol=None, covalent=False, self_ref=False, max_lig_len=0):
     '''Formats a PDB file into fragalysis friendly format.
     1. Validates the naming of the pdbs.
     2. It aligns the pdbs (_bound.pdb file).
@@ -16,7 +16,7 @@ def import_single_file(in_file, out_dir, target, monomerize, reference_pdb, biom
     :param in_file: Filepath of pdb you with to align (where additional files are stored in same directory)
     :param out_dir: Directory containing processed pdbs (will be created if it doesn't exists)
     :param target: Name of the folder to be created inside out_dir
-    :param monomerize: Bool, if True, will attempt to split pdb files into seperate chains
+    :param reduce_reference_frame: Bool, if True, will attempt to split pdb files into seperate chains
     :param reference_pdb: Name of the Reference pdb to align in_file to. If called from command line this will default to out_dir/target/reference.pdb
     :param biomol: plain-text file containing header information about the bio-molecular
         context of the pdb structures. If provided the contents will be appended to the top of the _apo.pdb files
@@ -37,71 +37,60 @@ def import_single_file(in_file, out_dir, target, monomerize, reference_pdb, biom
 
     # Experimental - if option is used then chains are converted.
     if int(max_lig_len) > int(0):
-        print(f'EXPERIMENTAL: Converting all chains with less than {max_lig_len} residues to HETATM LIG')
+        print(
+            f'EXPERIMENTAL: Converting all chains with less than {max_lig_len} residues to HETATM LIG')
         out = os.path.join(out_dir, f'maxliglen{target}/')
         if not os.path.isdir(out):
             os.makedirs(out)
-        convert_small_AA_chains(in_file=in_file, out_file=os.path.join(out, in_file), max_len=max_lig_len)
+        convert_small_AA_chains(in_file=in_file, out_file=os.path.join(
+            out, in_file), max_len=max_lig_len)
         copy_extra_files(in_file=in_file, out_dir=out)
         in_dir = out
 
-
-    if monomerize:
-        print("Monomerizing input structure")
-        out = os.path.join(out_dir, f'mono{target}/')
-        if not os.path.isdir(out):
-            os.makedirs(out)
-
-        mono = Monomerize(directory=in_dir, outdir=out)
-        mono.monomerize_single(file=in_file)
-        in_dir = out
-        for f in os.listdir(in_dir):
-            if '.pdb' in f:
-                pdb_smiles_dict['pdb'].append(os.path.join(in_dir, f))
-                if os.path.isfile(os.path.join(in_dir, f).replace('.pdb', '_smiles.txt')):
-                    pdb_smiles_dict['smiles'].append(os.path.join(in_dir, f).replace('.pdb', '_smiles.txt'))
-                else:
-                    pdb_smiles_dict['smiles'].append(None)
+    pdb_smiles_dict['pdb'].append(in_file)
+    if os.path.isfile(in_file.replace('.pdb', '_smiles.txt')):
+        pdb_smiles_dict['smiles'].append(
+            in_file.replace('.pdb', '_smiles.txt'))
     else:
-        pdb_smiles_dict['pdb'].append(in_file)
-        if os.path.isfile(in_file.replace('.pdb', '_smiles.txt')):
-            pdb_smiles_dict['smiles'].append(in_file.replace('.pdb', '_smiles.txt'))
-        else:
-            pdb_smiles_dict['smiles'].append(None)
+        pdb_smiles_dict['smiles'].append(None)
 
     print("Aligning to Reference")
-    structure = Align(in_dir, "", monomerize)
-    print('a1')
+    structure = Align(in_dir, "", rrf=reduce_reference_frame)
+
     for i in pdb_smiles_dict['pdb']:
         if self_ref:
-            structure.align_to_reference(i, i, out_dir=os.path.join(out_dir, f"tmp{target}"))
+            structure.align_to_reference(
+                i, i, out_dir=os.path.join(out_dir, f"tmp{target}"))
         else:
-            structure.align_to_reference(i, reference_pdb=reference_pdb, out_dir=os.path.join(out_dir, f"tmp{target}"))
-    print('a2')
+            structure.align_to_reference(
+                i, reference_pdb=reference_pdb, out_dir=os.path.join(out_dir, f"tmp{target}"))
+
     for smiles_file in pdb_smiles_dict['smiles']:
         if smiles_file:
             print(smiles_file)
-            shutil.copyfile(smiles_file, os.path.join(os.path.join(out_dir, f"tmp{target}", smiles_file.split('/')[-1])))
-            print(os.path.join(out_dir, f"tmp{target}", smiles_file.split('/')[-1]))
-    print('a3')
-    aligned_dict = {'bound_pdb': [], 'smiles': []}
+            shutil.copyfile(smiles_file, os.path.join(os.path.join(
+                out_dir, f"tmp{target}", smiles_file.split('/')[-1])))
+            print(os.path.join(
+                out_dir, f"tmp{target}", smiles_file.split('/')[-1]))
 
+    aligned_dict = {'bound_pdb': [], 'smiles': []}
     for f in os.listdir(os.path.join(out_dir, f"tmp{target}")):
         if '.pdb' in f:
-            aligned_dict['bound_pdb'].append(os.path.join(out_dir, f"tmp{target}", f))
+            aligned_dict['bound_pdb'].append(
+                os.path.join(out_dir, f"tmp{target}", f))
             if os.path.isfile(os.path.join(out_dir, f"tmp{target}", f).replace('_bound.pdb', '_smiles.txt')):
                 aligned_dict['smiles'].append(os.path.join(out_dir, f"tmp{target}", f).replace('_bound.pdb',
                                                                                                '_smiles.txt'))
             else:
                 aligned_dict['smiles'].append(None)
-    print('a4')
+
     for aligned, smiles in list(zip(aligned_dict['bound_pdb'], aligned_dict['smiles'])):
         try:
             if smiles:
                 _ = set_up(target_name=target,
                            infile=os.path.abspath(aligned),
                            out_dir=out_dir,
-                           monomerize=monomerize,
+                           rrf=reduce_reference_frame,
                            smiles_file=os.path.abspath(smiles),
                            biomol=biomol,
                            covalent=covalent,
@@ -111,7 +100,7 @@ def import_single_file(in_file, out_dir, target, monomerize, reference_pdb, biom
                 _ = set_up(target_name=target,
                            infile=os.path.abspath(aligned),
                            out_dir=out_dir,
-                           monomerize=monomerize,
+                           rrf=reduce_reference_frame,
                            biomol=biomol,
                            covalent=covalent,
                            keep_headers=self_ref)
@@ -121,7 +110,7 @@ def import_single_file(in_file, out_dir, target, monomerize, reference_pdb, biom
             for file in os.listdir(os.path.join(out_dir, f"tmp{target}")):
                 if str(aligned) in file:
                     os.remove(os.path.join(out_dir, f"tmp{target}", str(file)))
-    print('a5')
+
     # Write clever method to copy in_file
     dest_dir = os.path.join(out_dir, target, 'crystallographic')
     globstr = in_file.replace('.pdb', '*')
@@ -130,7 +119,8 @@ def import_single_file(in_file, out_dir, target, monomerize, reference_pdb, biom
         shutil.copy(file, dest_dir)
 
     # Time to use a for loop?
-    clean_up = [os.path.join(out_dir, f'maxliglen{target}'), os.path.join(out_dir, f'mono{target}'), os.path.join(out_dir, f'tmp{target}')]
+    clean_up = [os.path.join(out_dir, f'maxliglen{target}'), os.path.join(
+        out_dir, f'mono{target}'), os.path.join(out_dir, f'tmp{target}')]
     [shutil.rmtree(x) for x in clean_up if os.path.exists(x)]
 
     print("Files are now in a fragalysis friendly format!")
@@ -153,14 +143,17 @@ if __name__ == "__main__":
         required=True,
     )
     parser.add_argument(
-        "-m", "--monomerize", action="store_true", default=False, help="Monomerize input"
+        "-rrf", "--reduce_reference_frame", action="store_true", default=False, help="Reduce Reference Frame"
     )
 
     parser.add_argument("-t", "--target", help="Target name", required=True)
 
-    parser.add_argument('-r', '--reference_pdb', help='Reference PDB Structure', required=False, default=None)
-    parser.add_argument('-sr', '--self_reference', action="store_true", help='Include this flag if you want each pdb file to only align to itself', required=False, default=False)
-    parser.add_argument("-b", "--biomol_txt", help="Biomol Input txt file", required=False, default=None)
+    parser.add_argument('-r', '--reference_pdb',
+                        help='Reference PDB Structure', required=False, default=None)
+    parser.add_argument('-sr', '--self_reference', action="store_true",
+                        help='Include this flag if you want each pdb file to only align to itself', required=False, default=False)
+    parser.add_argument(
+        "-b", "--biomol_txt", help="Biomol Input txt file", required=False, default=None)
     parser.add_argument("-c",
                         "--covalent",
                         action="store_true",
@@ -178,7 +171,7 @@ if __name__ == "__main__":
 
     in_file = args["in_file"]
     out_dir = args["out_dir"]
-    monomerize = args["monomerize"]
+    reduce_reference_frame = args["reduce_reference_frame"]
     target = args["target"]
     biomol = args["biomol_txt"]
     covalent = args["covalent"]
@@ -197,17 +190,16 @@ if __name__ == "__main__":
         print("Using the default input directory ", out_dir)
 
     if not os.path.isfile(reference_pdb):
-        print(f'Cannot find file called {reference_pdb}, please make sure the path is correct (or specify another reference using -r)!')
+        print(
+            f'Cannot find file called {reference_pdb}, please make sure the path is correct (or specify another reference using -r)!')
     else:
         import_single_file(in_file=in_file,
                            out_dir=out_dir,
                            target=target,
-                           monomerize=monomerize,
+                           reduce_reference_frame=reduce_reference_frame,
                            reference_pdb=reference_pdb,
                            biomol=biomol,
                            covalent=covalent,
                            self_ref=self_ref,
                            max_lig_len=mll)
         print(f'File has been aligned to {reference_pdb}')
-
-
