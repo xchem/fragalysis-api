6fab15ea4b8053bb7a1122ee538858f0c115495c
matteoferla <matteo.ferla@gmail.com>
2022-12-05 11:19:28 +0000

    :traffic_light: Allow 'download' use only (w/o Gemmi)

--- fragalysis_api/__init__.py
+++ fragalysis_api/__init__.py
@@ -1,15 +1,23 @@
 from .xcglobalscripts.set_config import ConfigSetup
 
-from .xcimporter.validate import Validate, ValidatePDB
-from .xcimporter.conversion_pdb_mol import set_up, convert_small_AA_chains, copy_extra_files
-from .xcimporter.align import Align
-from .xcimporter.xc_utils import to_fragalysis_dir
-from .xcimporter.sites import Sites, contextualize_crystal_ligands
-from .xcimporter.xcimporter import xcimporter
-from .xcimporter.single_import import import_single_file
+try:
+    from .xcimporter.validate import Validate, ValidatePDB
+    from .xcimporter.conversion_pdb_mol import set_up, convert_small_AA_chains, copy_extra_files
+    from .xcimporter.xc_utils import to_fragalysis_dir
+    from .xcimporter.sites import Sites, contextualize_crystal_ligands
+    from .xcimporter.xcimporter import xcimporter
+    from .xcimporter.single_import import import_single_file
+    from .xcimporter.align import Align
+except ImportError:
+    import warnings
+    warnings.warn("The xcimporter functionality is unavailable without Gemmi", category=ImportWarning)
+
 from .xcextracter.getdata import GetTargetsData, GetMoleculesData, GetPdbData, GetMolgroupData
 from .xcextracter.frag_web_live import can_connect
 from .xcextracter.xcextracter import xcextracter
 
 from .xcanalyser.graphcreator import GraphRequest, xcgraphcreator
 from .xcanalyser.xcanalyser import xcanalyser
+
+
+
