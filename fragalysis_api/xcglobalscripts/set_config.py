e5c3bda4a6c192acea7af9827b2fbecd522c9ff0
matteoferla <matteo.ferla@gmail.com>
2022-12-05 12:30:06 +0000

    :reminder_ribbon: Added reminder

--- fragalysis_api/xcglobalscripts/set_config.py
+++ fragalysis_api/xcglobalscripts/set_config.py
@@ -4,6 +4,7 @@ import os
 
 def ConfigSetup():
     # use config parser to get settings from config.ini
+    # todo Do not read package files this way, use importlib/pkg_resources
     settings_file = os.path.join(os.path.dirname(__file__), "config.ini")
     settings = configparser.ConfigParser()
     settings._interpolation = configparser.ExtendedInterpolation()
