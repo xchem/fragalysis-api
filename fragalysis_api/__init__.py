be7480eb7d5ee3010c889885dcfa067627d0ab63
matteoferla <matteo.ferla@gmail.com>
2022-12-05 12:31:08 +0000

    :pencil: added note

--- fragalysis_api/__init__.py
+++ fragalysis_api/__init__.py
@@ -1,5 +1,6 @@
-from .xcglobalscripts.set_config import ConfigSetup
+from .xcglobalscripts.set_config import ConfigSetup  # this requires `config.ini` in the working directory
 
+# import into Fragalysis requires gemmi
 try:
     from .xcimporter.validate import Validate, ValidatePDB
     from .xcimporter.conversion_pdb_mol import set_up, convert_small_AA_chains, copy_extra_files
@@ -12,6 +13,7 @@ except ImportError:
     import warnings
     warnings.warn("The xcimporter functionality is unavailable without Gemmi", category=ImportWarning)
 
+# extract from Fragalysis does not require gemmi
 from .xcextracter.getdata import GetTargetsData, GetMoleculesData, GetPdbData, GetMolgroupData
 from .xcextracter.frag_web_live import can_connect
 from .xcextracter.xcextracter import xcextracter
